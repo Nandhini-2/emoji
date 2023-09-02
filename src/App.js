6c4978bc03ccdc183d6e35ce0ed3df4788403ae7
Nandhini A <Nandy123@>
2023-09-02 15:20:55 +0530

    emoji-search-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import Header from './Header';
+import emojipedia from './emoji';
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const [data, setData] = useState([]);
+
+  function handleSearch(event){
+    setSearchText(event.target.value)
+  }
+
+  useEffect(() => {
+    const newData = emojipedia.filter(emoji => emoji.keywords.toLowerCase().includes(searchText.toLowerCase()));
+    setData(newData)
+},[searchText])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header className=""/>
+    <div className="input-wrapper">
+      <input
+       className='seach-box'
+        type="text"
+        onChange={handleSearch}
+        value={searchText}
+        placeholder="Search here "
+      />
     </div>
-  );
-}
+    <div className='dictionary'>
+     <div>{data.map(emojipedia => emojipedia.symbol)}</div> 
+    </div>
+
+
+  </div>
+);
+};
+ 
 
 export default App;
---
+++ src/emoji.js
@@ -0,0 +1,77 @@
+const emojipedia = [
+  {
+    "title": "100",
+    "symbol": "💯",
+    "keywords":
+      "hundred points symbol symbol wow wow win win perfect perfect parties parties"
+  },
+  {
+    "title": "Grinning",
+    "symbol": "😀",
+    "keywords": "grinning face happy smiley emotion"
+  },
+  {
+    "title": "Sad",
+    "symbol": "😞",
+    "keywords": "disappointed face sad tired emotion"
+  },
+  {
+    "title": "Grin",
+    "symbol": "😁",
+    "keywords":
+      "grinning face with smile eyes happy silly smiley emotion emotion good"
+  },
+  {
+    "title": "Joy",
+    "symbol": "😂",
+    "keywords":
+      "face with tears of joy happy silly laugh emotion sarcastic "
+  },
+  {
+    "title": "Smiley",
+    "symbol": "😃",
+    "keywords":
+      "smile face with open mouth happy emotion emotion good good"
+  },
+  {
+    "title": "Angry",
+    "symbol": "😡",
+    "keywords":
+      "angry face mad emotion emotion"
+  },
+  {
+    "title": "Sweat Smile",
+    "symbol": "😅",
+    "keywords":
+      "smiling face with open mouth and cold sweat workout sweat emotion"
+  },
+  {
+    "title": "Laughing",
+    "symbol": "😆",
+    "keywords":
+      "smiling face with open mouth and tightly-closed eyes happy laugh emotion emotion"
+  },
+  {
+    "title": "Innocent",
+    "symbol": "😇",
+    "keywords": "smiling face with halo emotion"
+  },
+  {
+    "title": "Wink",
+    "symbol": "😉",
+    "keywords": "winking face silly  emotion"
+  },
+  {
+    "title": "Cry",
+    "symbol": "😭",
+    "keywords":
+      "loudly crying face sad cry emotion heartbreak heartbreak"
+  },
+  {
+    "title": "Fear",
+    "symbol": "😨",
+    "keywords": "fearful face surprised emotion"
+  }
+  ];
+
+  export default emojipedia;
